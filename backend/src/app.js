e0b63c1c2e840cb136811ce6ecdc6695a5312e4d
CodingWilsonA <wilson.audoin@gmail.com>
2022-05-24 16:45:05 +0200

    Added unhandled promise rejection and uncaught exception handdling

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-
+const userRoutes = require("./routes/user-routes");
 const app = express();
+
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/api/auth", userRoutes);
+
+process
+  .on("unhandledRejection", (err, promise) => {
+    console.error(err, "Unhandled rejection at promise: ", promise);
+  })
+  .on("uncaughtException", (err) => {
+    console.error("Uncaught exception: ", err);
+    process.exit(1);
+  });
 
 module.exports = app;
