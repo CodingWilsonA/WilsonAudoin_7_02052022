c3bc560400280f2a1fd140c9193d0d76d22a3d7c
CodingWilsonA <wilson.audoin@gmail.com>
2022-05-29 21:06:55 +0200

    Added default value to users auth_id

--- backend/tools/tables-model.js
+++ backend/tools/tables-model.js
@@ -4,7 +4,7 @@ let usersTable = `CREATE TABLE IF NOT EXISTS users (
 	last_name varchar(50),
 	email varchar(255) NOT NULL UNIQUE,
 	password varchar(255) NOT NULL,
-	auth_id INT NOT NULL,
+	auth_id INT NOT NULL DEFAULT '1',
 	PRIMARY KEY (user_id)
   );`;
 
