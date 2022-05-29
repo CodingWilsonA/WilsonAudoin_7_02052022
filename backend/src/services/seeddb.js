33906b530503a99c6fd835c8f502bee7cb67e843
CodingWilsonA <wilson.audoin@gmail.com>
2022-05-29 15:42:56 +0200

    Wrapped db query into a promise

--- backend/src/services/seeddb.js
+++ backend/src/services/seeddb.js
@@ -3,21 +3,25 @@ const usersTable = require("../models/tables-model");
 
 db.getConnection(function (err, conn) {
   if (err) {
-    console.error(err.message);
-    process.exit();
+    console.error("Seeddb failed to get db connection: ", err);
+    process.exit(1);
   }
   console.log("Successfully logged into database!");
 
-  conn.query(usersTable, function (err) {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
+  const userTableSeedPromise = new Promise((resolve) => {
+    conn.query(usersTable, function (err) {
+      if (err) {
+        console.error(err.message);
+      }
+      resolve();
+    });
   });
-  conn.release(function (err) {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
+  userTableSeedPromise.then(() => {
+    conn.release(function (err) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+    });
   });
 });
