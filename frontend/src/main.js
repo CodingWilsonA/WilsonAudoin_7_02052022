aabe5ab4d44f05aa81bb0dd1e1ec11256d7b5933
CodingWilsonA <wilson.audoin@gmail.com>
2022-05-30 17:37:58 +0200

    Fixed store path

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { sync } from "vuex-router-sync";
-import store from "./store";
+import store from "./store/index";
 
 Vue.config.productionTip = false;
 
