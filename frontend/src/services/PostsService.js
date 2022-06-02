380e967e4aabaaa786df1b9ec39553e5369dcdc8
CodingWilsonA <wilson.audoin@gmail.com>
2022-06-02 19:22:07 +0200

    Added frontend post creation layout

--- frontend/src/services/PostsService.js
+++ frontend/src/services/PostsService.js
@@ -1,7 +1,7 @@
 import Api from "./Api";
 
 export default {
-  getAllPosts() {
-    return Api().get("api/posts");
+  createPost(postData) {
+    return Api().post("api/posts", postData);
   },
 };
